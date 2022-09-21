02c58f68e6bbf90336e6906578c1ecb3d2832c1c
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-09-21 07:00:41 +0000

    Started authentiaction & connection process. For now, it will only be compatible with REPLIT database systems and does not yet have cross database function.s

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+#from src.server.user_connect import *
+from serverdb.connect import *
+
 ##
+print(connect())
+currentuserid = connectionid
+print(currentuserid)
---
+++ temparchive/src/server/user_connect.py
@@ -0,0 +1,8 @@
+from src.database import *
+
+userid = connect()
+
+def connect_user():
+    print(f"Connected. With user ID {userid}")
+    # return to now wait for messages to be sent/received
+    # execute code for that
